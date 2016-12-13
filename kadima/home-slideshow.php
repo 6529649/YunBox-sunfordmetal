962ce00d706cdcefc98855d6022bc6a05ace3117
q6529649 <234812835@qq.com>
2016-12-13 19:17:48 +0800

    Update

--- kadima/home-slideshow.php
+++ kadima/home-slideshow.php
@@ -1,5 +1,4 @@
-<!-- Carousel
-    ================================================== -->
+
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
       <div class="carousel-inner">
 	  <?php $wl_theme_options = kadima_get_options(); $j=1;
@@ -11,20 +10,13 @@
           <img src="<?php echo esc_url($wl_theme_options['slide_image_'.$i]); ?>" class="img-responsive" alt="<?php echo esc_attr($wl_theme_options['slide_title_'.$i]); ?>">
 		  <div class="container">
             <div class="carousel-caption">
-			<?php if($wl_theme_options['slide_title_'.$i]!='') {  ?>
 			<div class="carousel-text">
-            <h1 class="animated bounceInRight"><?php echo esc_attr($wl_theme_options['slide_title_'.$i]); ?></h1>
-			<?php
-			 if($wl_theme_options['slide_desc_'.$i]!='') {  ?>
-			  <ul class="list-unstyled carousel-list">
-			 <li class="animated bounceInLeft"><?php echo esc_attr($wl_theme_options['slide_desc_'.$i]); ?></li>
-			 </ul>
-			 <?php }
-			if($wl_theme_options['slide_btn_text_'.$i]!='') { ?>
-            <a class="kadima_blog_read_btn animated bounceInUp" href="<?php if($wl_theme_options['slide_btn_link_'.$i]!='') { echo esc_url($wl_theme_options['slide_btn_link_'.$i]); } ?>" role="button"><?php echo esc_attr($wl_theme_options['slide_btn_text_'.$i]); ?></a>
-			<?php } ?>
+				<?php if($wl_theme_options['slide_btn_text_'.$i]!='') { ?>
+					<a class="kadima_blog_read_btn animated bounceInUp" href="<?php if($wl_theme_options['slide_btn_link_'.$i]!='') { echo esc_url($wl_theme_options['slide_btn_link_'.$i]); } ?>" role="button">
+					<?php echo esc_attr($wl_theme_options['slide_btn_text_'.$i]); ?>
+					</a>
+				<?php } ?>
             </div>
-			<?php } ?>
 			</div>
           </div>
         </div>
@@ -37,4 +29,4 @@
 	</ol>
       <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
       <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-    </div><!-- /.carousel -->
+    </div>
